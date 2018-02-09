bbc62bcc0eaed2c11dc1700590113fb0aea9cdfa
be-bert <bebert.apps@gmail.com>
2018-02-09 09:22:58 -0500

    Add a grid material and scale it when the plane size changes.

--- ARPlaneDetector/Plane.swift
+++ ARPlaneDetector/Plane.swift
@@ -18,15 +18,14 @@ class Plane: SCNNode {
         
         super.init()
         
-        plane.cornerRadius = 0.005
+        plane.cornerRadius = 0.008
+        plane.materials = [GridMaterial()]
         
         let planeNode = SCNNode(geometry: plane)
         planeNode.position = SCNVector3Make(anchor.center.x, 0, anchor.center.z)
         
-        // Planes in SceneKit are vertical by default so we need to rotate 90degrees to match
+        // Planes in SceneKit are vertical by default so we need to rotate 90 degrees to match
         planeNode.eulerAngles.x = -.pi / 2
-        
-        planeNode.opacity = 0.15
         
         addChildNode(planeNode)
     }
@@ -39,7 +38,43 @@ class Plane: SCNNode {
         plane.width = CGFloat(anchor.extent.x)
         plane.height = CGFloat(anchor.extent.z)
         
+        if let grid = plane.materials.first as? GridMaterial {
+            grid.updateWith(anchor: anchor)
+        }
+        
         position = SCNVector3Make(anchor.center.x, 0, anchor.center.z)
+    }
+    
+}
+
+class GridMaterial: SCNMaterial {
+    
+    override init() {
+        super.init()
+        
+        let image = UIImage(named: "Grid")
+        
+        diffuse.contents = image
+        diffuse.wrapS = .repeat
+        diffuse.wrapT = .repeat
+    }
+    
+    required init?(coder aDecoder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    func updateWith(anchor: ARPlaneAnchor) {
+        
+        /*
+         Scene Kit uses meters for its measurements.
+         In order to get the texture looking good we need to decide the amount of times we want it to repeat per meter.
+         */
+        
+        let mmPerMeter: Float = 1000
+        let mmOfImage: Float = 65
+        let repeatAmount: Float = mmPerMeter / mmOfImage
+        
+        diffuse.contentsTransform = SCNMatrix4MakeScale(anchor.extent.x * repeatAmount, anchor.extent.z * repeatAmount, 1)
     }
     
 }
