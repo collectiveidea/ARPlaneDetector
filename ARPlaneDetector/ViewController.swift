45fefb9b2dd69468baa78861ef493d34136a8b46
be-bert <bebert.apps@gmail.com>
2018-02-27 14:12:53 -0500

    WIP: Shooting a projectile

--- ARPlaneDetector/ViewController.swift
+++ ARPlaneDetector/ViewController.swift
@@ -72,4 +72,49 @@ class ViewController: UIViewController {
         node.position = position
     }
 
+    @IBAction func tappedShoot(_ sender: Any) {
+        let camera = sceneView.session.currentFrame!.camera
+        let projectile = Projectile()
+        
+        // transform to location of camera
+        var translation = matrix_float4x4(projectile.transform)
+        translation.columns.3.z = -0.1
+        translation.columns.3.x = 0.03
+        
+        projectile.simdTransform = matrix_multiply(camera.transform, translation)
+        
+        let force = simd_make_float4(-3, 0, -8, 0)
+        let rotatedForce = simd_mul(camera.transform, force)
+        
+        let impulse = SCNVector3(rotatedForce.x, rotatedForce.y, rotatedForce.z)
+
+        sceneView?.scene.rootNode.addChildNode(projectile)
+        
+        projectile.launch(inDirection: impulse)
+    }
+    
+}
+
+class Projectile: SCNNode {
+    
+    override init() {
+        super.init()
+        
+        let capsule = SCNCapsule(capRadius: 0.006, height: 0.06)
+        
+        geometry = capsule
+        
+        eulerAngles = SCNVector3(CGFloat.pi / 2, (CGFloat.pi * 0.25), 0)
+        
+        physicsBody = SCNPhysicsBody(type: .dynamic, shape: nil)
+    }
+    
+    required init?(coder aDecoder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    func launch(inDirection direction: SCNVector3) {
+        physicsBody?.applyForce(direction, asImpulse: true)
+    }
+    
 }
